967e8121e98bf966554b759049a6a567e1436cf8
Christopher Wood <cmw2379@gmail.com>
2018-10-19 18:24:13 -0400

    Added stuff

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var port = 80;
 var express = require('express');
+var favicon = require('serve-favicon')
+var path = require('path')
 var app = express();
 
+app.use(favicon(path.join(__dirname, 'favicon.ico')))
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/scripts'));
+app.use(express.static(__dirname + '/images'));
+app.use(express.static(__dirname + '/style'));
 
 app.get('/', function (request, response) {
   response.sendFile('/index.html');
