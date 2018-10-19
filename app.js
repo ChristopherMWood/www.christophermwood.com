5200e06af37cca2e83f91fc466273227f262aac0
Christopher Wood <cmw2379@gmail.com>
2018-10-19 18:55:50 -0400

    corrected favicon configuration

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var port = 80;
 var express = require('express');
-var favicon = require('serve-favicon')
-var path = require('path')
 var app = express();
 
-app.use(favicon(path.join(__dirname, 'favicon.ico')))
+var favicon = require('serve-favicon')
+var path = require('path')
+
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/scripts'));
 app.use(express.static(__dirname + '/images'));
 app.use(express.static(__dirname + '/style'));
+app.use(express.static(__dirname + '/public'));
+app.use(favicon(path.join(__dirname, 'favicon.ico')))
 
 app.get('/', function (request, response) {
   response.sendFile('/index.html');
