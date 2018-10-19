e5b31c71e9e23c8100de4d0f66054c0f969994a7
Christopher Wood <cmw2379@gmail.com>
2018-10-19 15:10:39 -0400

    Cleaned up base style

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ app.get('/test', function (request, response) {
   response.send('testing');
 });
 
-app.use(function(req, res, next) {
-  return res.status(404).send({ message: 'Route'+req.url+' Not found.' });
+app.get('*', function(req, res){
+  res.status(404);
+  response.render('/404.html');
 });
 
 app.use(function(err, req, res, next) {
